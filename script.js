675dd99aab8aa88c1c87d54dac18b8f389a1b78e
BogBlog31 <bogblog31@gmail.com>
2025-10-13 03:42:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -312,6 +312,17 @@ function buildExpandablePopups(facilities, sections, images) {
     map.setView([lat, lng], 18, { animate: true });
   };
 }
+// Coordinate popup
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
 
 /**************************************************
  * Small helpers
@@ -323,4 +334,5 @@ function sanitizeHTML(str) {
 function escapeId(s) { return String(s).replace(/[^a-z0-9_\-]/gi, '_'); }
 function escapeJS(s) { return String(s).replace(/\\/g,'\\\\').replace(/'/g,"\\'").replace(/"/g,'\\"'); }
 function unescapeJS(s) { return String(s).replace(/\\'/g,"'").replace(/\\"/g,'"').replace(/\\\\/g,'\\'); }
+
 
