e18345a74b58c63ec718ae845b0798b732c36e9e
BogBlog31 <bogblog31@gmail.com>
2025-10-13 03:33:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,15 @@ const OLD_IMG_PATH = 'data/old_images.json';
  * Map initialization
  **************************************************/
 const map = L.map('map').setView([20, 0], 2);
+// Prevent clicks inside popups from closing them
+map.on('popupopen', function(e) {
+  const popupEl = e.popup.getElement();
+  if (popupEl) {
+    L.DomEvent.disableClickPropagation(popupEl);
+    L.DomEvent.disableScrollPropagation(popupEl);
+  }
+});
+// OpenStrretMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
@@ -314,3 +323,4 @@ function sanitizeHTML(str) {
 function escapeId(s) { return String(s).replace(/[^a-z0-9_\-]/gi, '_'); }
 function escapeJS(s) { return String(s).replace(/\\/g,'\\\\').replace(/'/g,"\\'").replace(/"/g,'\\"'); }
 function unescapeJS(s) { return String(s).replace(/\\'/g,"'").replace(/\\"/g,'"').replace(/\\\\/g,'\\'); }
+
